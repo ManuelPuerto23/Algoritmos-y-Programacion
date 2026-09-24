6eae111aa144f39076f500cca2d386776fdfc485
ManuelPuerto23 <manuelpuertosantos@gmail.com>
2026-09-24 08:29:14 -0600

    Add files via upload

---
+++ Apuntes 24.09.26/ejercicio.cpp
@@ -0,0 +1,50 @@
+//Manuel Antonio Puerto Santos
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+   //Definición de las variables
+   float salBas, porBon, valBon;
+   int tieSer
+
+    cout << "Salario basico :";
+    cin >> salBase
+    
+        cout << "Tiempo de servicio en años :";
+    cin >> tieSer
+    
+    if (tieSer < 5) {
+        porBon = 5;
+    }
+    else {
+        if (tieSer < 15){
+            porBon = 15;
+    }
+    else {
+        if (tieSer < 20) {
+            porBon = 20
+        }
+        else { 
+            if (tieser < 25) {
+                porBon = 25;
+            }
+            else{
+                if (tieSer < 30) {
+                    porBon = 35;
+                }
+                else {
+                    porBon = 50;
+                }
+            }
+         }
+     } 
+    }
+}
+ valBon = (salBas + porBon)/100;
+ 
+    //Datos de salida parciales
+    cout <<
+    return 0;
+}
